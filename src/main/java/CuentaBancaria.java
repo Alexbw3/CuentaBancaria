6659e15dfc02d8f8b84358d5629055b68668a105
alex lira barrios <22670046@itiguala.edu.mx>
2023-03-09 07:59:07 -0600

    se crea clase CuentaBancaria

---
+++ src/main/java/CuentaBancaria.java
@@ -0,0 +1,14 @@
+
+    import java.io.IOException;
+import java.util.Scanner;
+    public class CuentaBancaria {
+        public static void main(String[] args) throws IOException{
+
+            CuentaBBVA Account = new CuentaBBVA("Alexander",8989898,890);
+
+            Account.Menu();
+
+
+
+        }
+}
